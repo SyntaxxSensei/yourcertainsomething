add61d93c6d91b461fbc78b33e05e6ed58522ec4
Dean <2243399+SyntaxxSensei@users.noreply.github.com>
2024-09-26 20:41:45 +0100

    Fixed font sizes and spacing for desktop in Nav

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,18 +2,19 @@ import Image from 'next/image';
 
 export default function NavBar() {
     return (
-        <div className="flex flex-row">
+        <div className="flex flex-row p-5">
             <div className=" flex items-center">
                 <a href='/'>
                     <Image src={'/images/heartlogo.gif'} alt='logo' width="80" height="80" />
                 </a>
-                <p className="text-2xl p-5"><a href="/">Your Certain Something</a></p>
+                <p className="text-2xl font-bold p-5"><a href="/">Your Certain Something</a></p>
             </div>
-            <div className="flex items-center ml-auto pr-10">
+            <div className="flex items-center ml-auto pr-2">
                 <ul className="flex flex-row">
-                    <li className="p-3"><a href="/about">About</a></li>
-                    <li className="p-3"><a href="/projects">Projects</a></li>
-                    <li className="p-3"><a href="/contact">Contact</a></li>
+                    <li className="p-4 text-lg"><a href="/">Home</a></li>
+                    <li className="p-4 text-lg"><a href="/about">About</a></li>
+                    <li className="p-4 text-lg"><a href="/projects">Projects</a></li>
+                    <li className="p-4 text-lg"><a href="/contact">Contact</a></li>
                 </ul>
             </div>
         </div>

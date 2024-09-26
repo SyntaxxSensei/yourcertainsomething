58c529a18b5daef54cac8b79bda2dcfc4ff9c8c8
Dean <2243399+SyntaxxSensei@users.noreply.github.com>
2024-09-26 20:20:24 +0100

    Added logo to navbar with links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,21 @@
+import Image from 'next/image';
+
 export default function NavBar() {
     return (
-        <div>I AM NAVBAR!</div>
+        <div className="flex flex-row">
+            <div className=" flex items-center">
+                <a href='/'>
+                    <Image src={'/images/heartlogo.gif'} alt='logo' width="80" height="80" />
+                </a>
+                <p className="text-2xl p-8"><a href="/">Your Certain Something</a></p>
+            </div>
+            <div className="flex items-center ml-auto pr-10">
+                <ul className="flex flex-row">
+                    <li className="p-3"><a href="/about">About</a></li>
+                    <li className="p-3"><a href="/projects">Projects</a></li>
+                    <li className="p-3"><a href="/contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
     );
 }

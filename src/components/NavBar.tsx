81e37059683054c7f433a8b6b1319cb94e4d33c0
Dean <2243399+SyntaxxSensei@users.noreply.github.com>
2024-09-26 20:23:19 +0100

    Updated padding on navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
                 <a href='/'>
                     <Image src={'/images/heartlogo.gif'} alt='logo' width="80" height="80" />
                 </a>
-                <p className="text-2xl p-8"><a href="/">Your Certain Something</a></p>
+                <p className="text-2xl p-5"><a href="/">Your Certain Something</a></p>
             </div>
             <div className="flex items-center ml-auto pr-10">
                 <ul className="flex flex-row">

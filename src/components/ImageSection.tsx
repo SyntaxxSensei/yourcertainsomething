f8042ee51dcfad7cd46165fb3c14c9d4f2b975d3
Dean <2243399+SyntaxxSensei@users.noreply.github.com>
2024-10-01 23:28:16 +0100

    Adjusted image sizing

--- src/components/ImageSection.tsx
+++ src/components/ImageSection.tsx
@@ -6,12 +6,16 @@ export default function ImageSection() {
             <div className="flex text-xl md:text-3xl xl:text-4xl justify-center">
                 <p>Why invest in gamification?</p>
             </div>
-            <div className="flex justify-center mt-5 gap-x-4 max-w-[700px] p-4">
-                <Image className="w-1/5"  src={'/images/more-engaging.png'} alt='logo' width="150" height="150" />
-                <Image className="w-1/5" src={'/images/more-useable.png'} alt='logo' width="150" height="150" />
-                <Image className="w-1/5" src={'/images/healthy-competition.png'} alt='logo' width="150" height="150" />
-                <Image className="w-1/5" src={'/images/realtime-feedback.png'} alt='logo' width="150" height="150" />
-                <Image className="w-1/5" src={'/images/more-unique.webp'} alt='logo' width="150" height="150" />
+            <div className="flex md:flex-row flex-col justify-center mt-5 p-4">
+                <div className="flex justify-center">
+                    <Image className="w-auto" src={'/images/more-engaging.png'} alt='logo' width="150" height="150" />
+                    <Image className="w-auto" src={'/images/more-useable.png'} alt='logo' width="150" height="150" />
+                    <Image className="w-auto" src={'/images/healthy-competition.png'} alt='logo' width="150" height="150" />
+                </div>
+                <div className="flex justify-center">
+                    <Image className="w-auto" src={'/images/realtime-feedback.png'} alt='logo' width="150" height="150" />
+                    <Image className="w-auto" src={'/images/more-unique.webp'} alt='logo' width="150" height="150" />
+                </div>
             </div>
             <div className="border-b-2 border-grey-200 m-6"></div>
         </div>

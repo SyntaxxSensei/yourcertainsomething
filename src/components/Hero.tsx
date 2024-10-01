07c51c05d7bb50f501b53549dab9abd223426a3e
Dean <2243399+SyntaxxSensei@users.noreply.github.com>
2024-10-01 23:12:44 +0100

    Enhanced styling for hero and image section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,14 @@
 export default function Hero() {
     return (
         <div className="bg-hero h-[500px] md:h-[600px] flex">
-            <div className="flex flex-col justify-center p-8 md:p-24 w-[500px]">
-                <span className="text-5xl text-white pb-6">Personalised game design.</span>
-                <span className="text-lg text-white pb-6">Enhance communication, collaboration, cognition and retention through intuitive game design.</span>
-                <span className="flex">
-                    <a href="/projects" className="border-2 border-black p-4 text-black hover:text-white hover:bg-black">See what&apos;s possible</a>
-                </span>
+            <div className="max-w-7xl w-full mx-auto flex items-center">
+                <div className="flex flex-col justify-center pl-8 pr-8 md:pl-24 py-24 max-w-[500px]">
+                    <span className="text-3xl md:text-5xl text-white pb-6">Personalised game design.</span>
+                    <span className="text-lg text-white pb-6">Enhance communication, collaboration, cognition and retention through intuitive game design.</span>
+                    <span className="flex">
+                        <a href="/projects" className="border-2 border-black p-4 text-black hover:text-white hover:bg-black">See what&apos;s possible</a>
+                    </span>
+                </div>
             </div>
         </div>
     );
--- src/components/ImageSection.tsx
+++ src/components/ImageSection.tsx
@@ -2,16 +2,16 @@ import Image from 'next/image';
 
 export default function ImageSection() {
     return (
-        <div className="flex flex-col m-10">
-            <div className="flex text-3xl justify-center">
+        <div className="flex flex-col m-10 items-center">
+            <div className="flex text-xl md:text-3xl xl:text-4xl justify-center">
                 <p>Why invest in gamification?</p>
             </div>
-            <div className="flex flex-row justify-center gap-x-8 mt-5">
-                <Image src={'/images/more-engaging.png'} alt='logo' width="90" height="90" />
-                <Image src={'/images/more-useable.png'} alt='logo' width="90" height="90" />
-                <Image src={'/images/healthy-competition.png'} alt='logo' width="90" height="90" />
-                <Image src={'/images/realtime-feedback.png'} alt='logo' width="90" height="90" />
-                <Image src={'/images/more-unique.webp'} alt='logo' width="90" height="90" />
+            <div className="flex justify-center mt-5 gap-x-4 max-w-[700px] p-4">
+                <Image className="w-1/5"  src={'/images/more-engaging.png'} alt='logo' width="150" height="150" />
+                <Image className="w-1/5" src={'/images/more-useable.png'} alt='logo' width="150" height="150" />
+                <Image className="w-1/5" src={'/images/healthy-competition.png'} alt='logo' width="150" height="150" />
+                <Image className="w-1/5" src={'/images/realtime-feedback.png'} alt='logo' width="150" height="150" />
+                <Image className="w-1/5" src={'/images/more-unique.webp'} alt='logo' width="150" height="150" />
             </div>
             <div className="border-b-2 border-grey-200 m-6"></div>
         </div>
